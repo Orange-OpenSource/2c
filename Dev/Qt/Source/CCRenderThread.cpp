edc0b111187820ade8f72d243707aef8182dfb25
Ash Cairo <ashcairo@gmail.com>
2011-10-15 14:39:32 +0100

    Added Qt port.

---
+++ Dev/Qt/Source/CCRenderThread.cpp
@@ -0,0 +1,45 @@
+/*-----------------------------------------------------------
+ * 2C - Cross Platform 3D Application Framework
+ *-----------------------------------------------------------
+ * Copyright © 2011 – 2011 France Telecom
+ * This software is distributed under the Apache 2.0 license,
+ * see the "license.txt" file for more details.
+ *-----------------------------------------------------------
+ * File Name   : CCRenderThread.cpp
+ *-----------------------------------------------------------
+ */
+
+#include "CCDefines.h"
+#include "CCGLView.h"
+#include "CCRenderThread.h"
+
+
+QGLRenderThread::QGLRenderThread(CCGLView *parent) :
+    QThread(),
+    glView( parent )
+{
+}
+
+void QGLRenderThread::resizeViewport(const QSize &size)
+{
+    w = size.width();
+    h = size.height();
+}
+
+
+void QGLRenderThread::run()
+{
+    glView->makeCurrent();
+
+    gEngine = new CCAppEngine();
+    gEngine->setupNativeThread();
+    gEngine->setupGameThread();
+
+    do
+    {
+        gEngine->updateNativeThread();
+        gEngine->updateGameThread();
+
+        glView->swapBuffers();
+    } while( glView->runningGame );
+}
